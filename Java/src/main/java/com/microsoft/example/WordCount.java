3f5ea7df8355ec8fee6f8f8119f6905a9b547fc5
Larry Franks <larry.franks@microsoft.com>
2016-10-13 13:34:10 -0400

    refactoring Java example for Storm 1.0.1

--- Java/src/main/java/com/microsoft/example/WordCount.java
+++ Java/src/main/java/com/microsoft/example/WordCount.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Iterator;
 
-import backtype.storm.Constants;
-import backtype.storm.topology.BasicOutputCollector;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseBasicBolt;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
-import backtype.storm.Config;
+import org.apache.storm.Constants;
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.Config;
 
 // For logging
 import org.apache.logging.log4j.Logger;
--- Java/src/main/java/com/microsoft/example/WordCountTopology.java
+++ Java/src/main/java/com/microsoft/example/WordCountTopology.java
@@ -1,10 +1,10 @@
 package com.microsoft.example;
 
-import backtype.storm.Config;
-import backtype.storm.LocalCluster;
-import backtype.storm.StormSubmitter;
-import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.tuple.Fields;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
 
 import com.microsoft.example.RandomSentenceSpout;
 
